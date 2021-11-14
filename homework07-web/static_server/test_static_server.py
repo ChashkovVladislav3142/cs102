d92125b828795acb0df4e9ce60aba76b191871a0
ChashkovVladislav3142 <70941464+ChashkovVladislav3142@users.noreply.github.com>
2021-11-14 04:59:09 +0300

    Update test_static_server.py

--- homework07-web/static_server/test_static_server.py
+++ homework07-web/static_server/test_static_server.py
@@ -199,3 +199,22 @@ class TestStaticHTTPServer(unittest.TestCase):
         ctype = r.headers.get("Content-Type")
         self.assertEqual(ctype, "image/png")
 
+
+# loader = unittest.TestLoader()
+# suite = unittest.TestSuite()
+# a = loader.loadTestsFromTestCase(TestStaticHTTPServer)
+# suite.addTest(a)
+#
+#
+# class NewResult(unittest.TextTestResult):
+#     def getDescription(self, test):
+#         doc_first_line = test.shortDescription()
+#         return doc_first_line or ""
+#
+#
+# class NewRunner(unittest.TextTestRunner):
+#     resultclass = NewResult
+#
+#
+# runner = NewRunner(verbosity=2)
+# runner.run(suite)
